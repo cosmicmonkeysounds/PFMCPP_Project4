3fd4467652b6ba8821a569e8f6c69ad6b37a119a
John Janigan-Mills <cosmicmonkeysounds@gmail.com>
2020-04-19 20:25:05 +0000

    Answered questions to Part4

--- main.cpp
+++ main.cpp
@@ -57,23 +57,35 @@ send me a DM to check your pull request
  Wait for my code review.
  */
 
+#include <iostream>
+#include <cmath>
+
+struct IntType; struct FloatType; struct DoubleType;
+
 struct Point
 {
-    Point& multiply(float m)
+    Point( float x_, float y_ ) : x(x_), y(y_) {}
+    Point( const IntType& );
+    Point( const FloatType& );
+    Point( const DoubleType& );
+
+    Point& multiply( float m )
     {
         x *= m;
         y *= m;
         return *this;
     }
+
+    Point& multiply( IntType& );
+    Point& multiply( FloatType& );
+    Point& multiply( DoubleType& );
+
+    void toString();
+
 private:
     float x{0}, y{0};
+
 };
-
-
-
- 
-
-#include <iostream>
 
 struct IntType
 {
@@ -86,18 +98,23 @@ struct IntType
     ~IntType()
     {
         delete value;
+        value = nullptr;
     }
 
-    IntType& add( int y );
-    IntType& subtract( int y );
-    IntType& multiply( int y );
-    IntType& divide( int y );
+    IntType& add     ( int );
+    IntType& subtract( int );
+    IntType& multiply( int );
+    IntType& divide  ( int );
+    IntType& pow( int );
+    IntType& pow( const IntType& );
+    IntType& pow( const FloatType& );
+    IntType& pow( const DoubleType& );
 
-    operator int() { return *value; }
+    operator int() const { return *value; }
 
 private:
     int *value = nullptr;
-
+    IntType& powInternal( const int );
 };
 
 struct FloatType
@@ -111,18 +128,24 @@ struct FloatType
     ~FloatType()
     {
         delete value;
+        value = nullptr;
     }
 
-    FloatType& add( float y );
-    FloatType& subtract( float y );
-    FloatType& multiply( float y );
-    FloatType& divide( float y );
+    FloatType& add     ( float );
+    FloatType& subtract( float );
+    FloatType& multiply( float );
+    FloatType& divide  ( float );
 
-    operator float() { return *value; }
+    FloatType& pow( float );
+    FloatType& pow( const IntType& );
+    FloatType& pow( const FloatType& );
+    FloatType& pow( const DoubleType& );
+
+    operator float() const { return *value; }
 
 private:
     float *value = nullptr;
-
+    FloatType& powInternal( const float );
 };
 
 struct DoubleType
@@ -136,37 +159,28 @@ struct DoubleType
     ~DoubleType()
     {
         delete value;
+        value = nullptr;
     }
 
-    DoubleType& add( double y );
-    DoubleType& subtract( double y );
-    DoubleType& multiply( double y );
-    DoubleType& divide( double y );
+    DoubleType& add     ( double );
+    DoubleType& subtract( double );
+    DoubleType& multiply( double );
+    DoubleType& divide  ( double );
+    DoubleType& pow( double );
+    DoubleType& pow( const IntType& );
+    DoubleType& pow( const FloatType& );
+    DoubleType& pow( const DoubleType& );
 
-    operator double() { return *value; }
+    operator double() const { return *value; }
 
 private:
     double *value = nullptr;
-
+    DoubleType& powInternal( const double );
 };
 
-IntType& IntType::add( int y )
-{   
-    *value += y;
-    return *this;
-}
-
-IntType& IntType::subtract( int y )
-{
-    *value -= y;
-    return *this;
-}
-
-IntType& IntType::multiply( int y )
-{   
-    *value *= y;
-    return *this;
-}
+IntType& IntType::add     ( int y ) { *value += y; return *this; }
+IntType& IntType::subtract( int y ) { *value -= y; return *this; }
+IntType& IntType::multiply( int y ) { *value *= y; return *this; }
 
 IntType& IntType::divide( int y )
 {
@@ -184,23 +198,20 @@ IntType& IntType::divide( int y )
     return *this;
 }
 
-FloatType& FloatType::add( float y )
-{
-    *value += y;
+IntType& IntType::powInternal( const int y )
+{   
+    if( value ) { *value = static_cast<int>( std::pow(*value, y) ); }
     return *this;
 }
 
-FloatType& FloatType::subtract( float y )
-{
-    *value -= y;
-    return *this;
-}
+IntType& IntType::pow( int y )                { return powInternal( y ); }
+IntType& IntType::pow( const IntType& it )    { return powInternal( static_cast<int>(it) ); }
+IntType& IntType::pow( const FloatType& ft )  { return powInternal( static_cast<int>(ft) ); }
+IntType& IntType::pow( const DoubleType& dt ) { return powInternal( static_cast<int>(dt) ); }
 
-FloatType& FloatType::multiply( float y )
-{
-    *value *= y;
-    return *this;
-}
+FloatType& FloatType::add     ( float y ) { *value += y; return *this; }
+FloatType& FloatType::subtract( float y ) { *value -= y; return *this; }
+FloatType& FloatType::multiply( float y ) { *value *= y; return *this; }
 
 FloatType& FloatType::divide( float y )
 {
@@ -212,24 +223,20 @@ FloatType& FloatType::divide( float y )
     return *this;
 }
 
-DoubleType& DoubleType::add( double y )
-{
-    *value += y;
+FloatType& FloatType::powInternal( const float y )
+{   
+    if( value ) { *value = static_cast<float>( std::pow(*value, y) ); }
     return *this;
 }
 
+FloatType& FloatType::pow( float y )              { return powInternal( y ); }
+FloatType& FloatType::pow( const IntType& it )    { return powInternal( static_cast<float>(it) ); }
+FloatType& FloatType::pow( const FloatType& ft )  { return powInternal( static_cast<float>(ft) ); }
+FloatType& FloatType::pow( const DoubleType& dt ) { return powInternal( static_cast<float>(dt) ); }
 
-DoubleType& DoubleType::subtract( double y )
-{
-    *value -= y;
-    return *this;
-}
-
-DoubleType& DoubleType::multiply( double y )
-{
-    *value *= y;
-    return *this;
-}
+DoubleType& DoubleType::add     ( double y ) { *value += y; return *this; }
+DoubleType& DoubleType::subtract( double y ) { *value -= y; return *this; }
+DoubleType& DoubleType::multiply( double y ) { *value *= y; return *this; }
 
 DoubleType& DoubleType::divide( double y )
 {
@@ -241,9 +248,29 @@ DoubleType& DoubleType::divide( double y )
     return *this;
 }
 
+DoubleType& DoubleType::powInternal( const double y )
+{   
+    if( value ) { *value = static_cast<double>( std::pow(*value, y) ); }
+    return *this;
+}
+
+DoubleType& DoubleType::pow( double y )             { return powInternal( y ); }
+DoubleType& DoubleType::pow( const IntType& it )    { return powInternal( static_cast<double>(it) ); }
+DoubleType& DoubleType::pow( const FloatType& ft )  { return powInternal( static_cast<double>(ft) ); }
+DoubleType& DoubleType::pow( const DoubleType& dt ) { return powInternal( static_cast<double>(dt) ); }
+
+Point::Point( const FloatType& ft )  : Point( ft, ft ) {}
+Point::Point( const IntType& it )    : Point( static_cast<float>(it), static_cast<float>(it) ) {}
+Point::Point( const DoubleType& dt ) : Point( static_cast<float>(dt), static_cast<float>(dt) ) {}
+
+Point& Point::multiply( IntType& it ) { return multiply( static_cast<float>(it) ); }
+Point& Point::multiply( FloatType& ft ) { return multiply( static_cast<float>(ft) ); }
+Point& Point::multiply( DoubleType& dt ) { return multiply( static_cast<float>(dt) ); }
+
+void Point::toString() { std::cout << "\nPoint Coords:\nX: " << x << "\nY: " << y << "\n"; }
+
 int main()
 {
-
     IntType it( 3 );
     it.add( 2 );
     int i = it;
@@ -255,7 +282,6 @@ int main()
     i = it.multiply(2).divide(3).add(1).subtract(100);
     std::cout << "multiplied by 2, divided by 3, add 1, subtract 100 is:  " << i << "\n\n";
 
-
     IntType anotherInt(1);
     FloatType ft(2.5f);
     DoubleType dt(1.5);
@@ -265,6 +291,23 @@ int main()
 
     DoubleType anotherDouble( 10.2 );
     double d = anotherDouble.divide( static_cast<double>( FloatType(5.f)) ).add( static_cast<double>(IntType(2)) );
-    std::cout << "10.2 divide by 5.f plus 2 is: " << d << "\n";
+    std::cout << "10.2 divide by 5.f plus 2 is: " << d << "\n\n";
 
+    IntType powInt(2);
+    int pit = powInt.pow(4);
+    std::cout << "2 ^ 4 is " << pit;
+    pit = powInt.pow( ft );
+    std::cout << ", and that to the power of 2.5 is: " << pit << "\n";
+
+    FloatType powF(2.4f);
+    float fit = powF.pow(2.f).pow(anotherDouble);
+    std::cout << "and (2.4 ^ 2) ^ 4.04 is: " << fit << "\n";
+
+    Point p(2, 2);
+    p.toString();
+    p.multiply(powF);
+    p.toString();
+    p.multiply(3);
+    p.toString();
+    std::cout << "\n";
 }
